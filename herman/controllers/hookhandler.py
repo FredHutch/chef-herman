d382d3160f4a8098301cc28244230df6360ff01b
Michael Gutteridge <mrg@fhcrc.org>
2016-04-06 14:17:40 -0700

    added default branch to data in job

--- herman/controllers/hookhandler.py
+++ herman/controllers/hookhandler.py
@@ -3,25 +3,38 @@ from flask import Blueprint, render_template
 from herman.extensions import hook
 from herman.extensions import redis_store
 
+from datetime import datetime
+
 hookhandler = Blueprint('hookhandler', __name__)
 
 @hookhandler.route('/hooks', methods=['POST'])
 @hook.hook('push')
 def go(data, delivery):
-    print "Data is {}".format( data )
-    print "Delivery is {}".format( delivery )
+    # Empty commits are most likely tags
+    if len( data['commits']  ) == 0:
+        print( "Skipping empty push to ref {}".format( data['ref'] ) )
+        return 'Skipped'
 
-    commit, ref, reponame = [
+    timestamp = datetime.today().strftime('%y-%m-%dT%H:%M:%S')
+    #print "Data is {}".format( data )
+    #print "Delivery is {}".format( delivery )
+
+    commit, ref, name, url, default_branch = [
         data['head_commit']['id'],
         data['ref'],
-        data['repository']['full_name']
+        data['repository']['name'],
+        data['repository']['url'],
+        data['repository']['default_branch']
     ]
     print(
         'New push {} to branch {} in repo {}'.format(
-            commit, ref, reponame
+            commit, ref, name
         )
     )
-    redis_store.lpush( 'herman', (commit, ref, reponame ) )
+    redis_store.lpush(
+        'herman',
+        ( timestamp, commit, ref, name, url, default_branch )
+    )
 
     return 'Thanks'
 
